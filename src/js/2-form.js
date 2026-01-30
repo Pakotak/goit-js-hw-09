3acc22c740f9354707738bbeaec1cefd9b5408b3
Pako_Tako <pavel.korschov@gmail.com>
2026-01-30 13:41:42 +0100

    complete HW-09 gallery and feedback form with localStorage

---
+++ src/js/2-form.js
@@ -0,0 +1,58 @@
+
+const form = document.querySelector(".feedback-form");
+const STORAGE_KEY = "feedback-form-state";
+
+const formData = {
+  email: "",
+  message: "",
+};
+
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  try {
+    const parsedData = JSON.parse(savedData);
+
+    
+    formData.email = parsedData.email ?? "";
+    formData.message = parsedData.message ?? "";
+
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+  } catch (error) {
+    
+    localStorage.removeItem(STORAGE_KEY);
+  }
+}
+
+
+form.addEventListener("input", event => {
+  const { name, value } = event.target;
+
+  
+  if (name !== "email" && name !== "message") return;
+
+  formData[name] = value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const email = formData.email.trim();
+  const message = formData.message.trim();
+
+  if (email === "" || message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log({ email, message });
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+
+  formData.email = "";
+  formData.message = "";
+});
